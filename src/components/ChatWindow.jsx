72d792029f2bd6edc02ca141d5774db39adb62c9
p-soham11 <sohampal1108@gmail.com.com>
2023-12-14 03:22:12 +0530

    Progressing with the UI

---
+++ src/components/ChatWindow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatWindow() {
+  return (
+    <div className='chatWindow'>
+      ChatWindow
+    </div>
+  )
+}
+
+export default ChatWindow
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatWindow from '../components/ChatWindow'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='container'>
+            <Sidebar />
+            <ChatWindow />
+        </div>
+    </div>
+  )
+}
+
+export default Home
